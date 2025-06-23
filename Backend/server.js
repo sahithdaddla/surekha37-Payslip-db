080b6572e613fa0544e080e3e7736091bdbd70e0
sahithdaddla <daddlasahith>
2025-06-23 16:37:02 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -114,5 +114,5 @@ app.get('/api/payslips/:empCode', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://13.48.195.115:${port}`);
+    console.log(`Server running on http://16.171.151.154:${port}`);
 });
