e5f5fcf9ca07a4a2d9325ad37a3658ebf94f36b9
sahithdaddla <daddlasahith>
2025-06-23 16:56:48 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3025;
+const port = 3035;
 
 // PostgreSQL database connection
 const pool = new Pool({
